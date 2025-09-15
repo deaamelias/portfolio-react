8b502648ecd16e2db15adc64a67ed786f9fed677
deaamelias <deamelyaa07@gmail.com>
2025-09-16 01:05:06 +0700

    feat: implement about, tools, and project sections
    - Added "Tentang" section with profile image and short introduction
    - Displayed total completed projects with styled stats
    - Implemented tools section with responsive grid layout and tool details
    - Added project section showcasing several portfolio items with stack labels and GitHub link
    - Used lazy loading for images to improve performance
    - Applied responsive design using Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import DataImage from "./data";
+import { listTools, listProyek } from "./data";
 
 function App() {
   return (
@@ -37,6 +38,7 @@ function App() {
           src={DataImage.HeroImage}
           alt="Hero Image"
           className="w-[500px] md:ml-auto"
+          loading="lazy"
         />
       </div>
 
@@ -46,6 +48,7 @@ function App() {
             src={DataImage.HeroImage}
             alt="Image"
             className="w-12 rounded-md mb-10 sm:hidden"
+            loading="lazy"
           />
           <p className="text-base/loose mb-10">
             Hi, perkenalkan saya Dea Amelia Setyorini, seorang Full Stack Web
@@ -59,6 +62,7 @@ function App() {
               src={DataImage.HeroImage}
               alt="Image"
               className="w-12 rounded-md sm:block hidden"
+              loading="lazy"
             />
             <div className="flex items-center gap-6">
               <div>
@@ -69,6 +73,69 @@ function App() {
               </div>
             </div>
           </div>
+        </div>
+
+        <div className="tools mt-32">
+          <h1 className="text-4xl/snug font-bold mb-4">Tools yang dipakai</h1>
+          <p className="xl:w-2/5 lg:w-2/4 md:w-2/3 sm:w-3/4 w-full text-base/loose opacity-50">
+            Berikut ini beberapa tools yang biasa saya pakai untuk pembuatan
+            Website
+          </p>
+          <div className="tools-box mt-14 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+            {listTools.map((tool) => (
+              <div
+                className="flex items-center gap-2 p-3 border border-zinc-600 rounded-md hover:bg-zinc-800 group"
+                key={tool.id}
+              >
+                <img
+                  src={tool.gambar}
+                  alt="Tools Image"
+                  className="w-14 bg-zinc-800 p-1 group-hover:bg-zinc-900"
+                  loading="lazy"
+                />
+                <div>
+                  <h4 className="font-bold">{tool.nama}</h4>
+                  <p className="opacity-50">{tool.ket}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="proyek mt-32 py-10">
+        <h1 className="text-center text-4xl font-bold mb-2">Proyek</h1>
+        <p className="text-base/loose text-center opacity-50">
+          Berikut ini beberapa proyek yang telah saya buat
+        </p>
+        <div className="proyek-box mt-14 grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+          {listProyek.map((proyek) => (
+            <div key={proyek.id} className="p-4 bg-zinc-800 rounded-md">
+              <img src={proyek.gambar} alt="Proyek Image" loading="lazy" />
+              <div>
+                <h1 className="text-2xl font-bold my-4">{proyek.nama}</h1>
+                <p className="text-base/loose mb-4">{proyek.desk}</p>
+                <div className="flex flex-wrap gap-2">
+                  {proyek.tools.map((tool, index) => (
+                    <p
+                      className="py-1 px-3 border border-zinc-500 bg-zinc-600 rounded-md font-semibold"
+                      key={index}
+                    >
+                      {tool}
+                    </p>
+                  ))}
+                </div>
+                <div className="mt-8 text-center">
+                  <a
+                    className="bg-violet-700 p-3 rounded-lg block border border-zinc-600 hover:bg-violet-600"
+                    href="#"
+                  >
+                    Github
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
--- src/data.js
+++ src/data.js
@@ -11,12 +11,23 @@ import Tools2 from "/assets/tools/reactjs.png";
 import Tools3 from "/assets/tools/nextjs.png";
 import Tools4 from "/assets/tools/tailwind.png";
 import Tools5 from "/assets/tools/bootstrap.png";
-import Tools6 from "/assets/tools/js.png";
+import Tools6 from "/assets/tools/idea.png";
 import Tools7 from "/assets/tools/nodejs.png";
-import Tools8 from "/assets/tools/github.png";
-import Tools9 from "/assets/tools/ai.png";
-import Tools10 from "/assets/tools/canva.png";
-import Tools11 from "/assets/tools/figma.png";
+import Tools8 from "/assets/tools/js.png";
+import Tools9 from "/assets/tools/python.png";
+import Tools10 from "/assets/tools/java.png";
+import Tools11 from "/assets/tools/php.png";
+import Tools12 from "/assets/tools/html.png";
+import Tools13 from "/assets/tools/css.png";
+import Tools14 from "/assets/tools/mysql.png";
+import Tools15 from "/assets/tools/xampp.png";
+import Tools16 from "/assets/tools/protege.png";
+import Tools17 from "/assets/tools/github.png";
+import Tools18 from "/assets/tools/gitlab.png";
+import Tools19 from "/assets/tools/postman.png";
+import Tools20 from "/assets/tools/canva.png";
+import Tools21 from "/assets/tools/figma.png";
+import Tools22 from "/assets/tools/vue.png";
 
 export const listTools = [
   {
@@ -57,8 +68,8 @@ export const listTools = [
   {
     id: 6,
     gambar: Tools6,
-    nama: "Javascript",
-    ket: "Language",
+    nama: "IntelliJ IDEA",
+    ket: "IDE",
     dad: "600",
   },
   {
@@ -71,30 +82,107 @@ export const listTools = [
   {
     id: 8,
     gambar: Tools8,
-    nama: "Github",
-    ket: "Repository",
+    nama: "Javascript",
+    ket: "Language",
     dad: "800",
   },
   {
     id: 9,
     gambar: Tools9,
-    nama: "Adobe Illustrator",
-    ket: "Design App",
+    nama: "Python",
+    ket: "Language",
     dad: "900",
   },
   {
     id: 10,
     gambar: Tools10,
-    nama: "Canva",
-    ket: "Design App",
+    nama: "Java",
+    ket: "Language",
     dad: "1000",
   },
   {
     id: 11,
     gambar: Tools11,
+    nama: "PHP",
+    ket: "Language",
+    dad: "1100",
+  },
+  {
+    id: 12,
+    gambar: Tools12,
+    nama: "HTML",
+    ket: "Markup Language",
+    dad: "1200",
+  },
+  {
+    id: 13,
+    gambar: Tools13,
+    nama: "CSS",
+    ket: "Style Sheet Language",
+    dad: "1300",
+  },
+  {
+    id: 14,
+    gambar: Tools14,
+    nama: "MySQL",
+    ket: "Database",
+    dad: "1400",
+  },
+  {
+    id: 15,
+    gambar: Tools15,
+    nama: "XAMPP",
+    ket: "Local Server",
+    dad: "1500",
+  },
+  {
+    id: 16,
+    gambar: Tools16,
+    nama: "Protégé",
+    ket: "Ontology Editor",
+    dad: "1600",
+  },
+  {
+    id: 17,
+    gambar: Tools17,
+    nama: "GitHub",
+    ket: "Repository",
+    dad: "1700",
+  },
+  {
+    id: 18,
+    gambar: Tools18,
+    nama: "GitLab",
+    ket: "Repository",
+    dad: "1800",
+  },
+  {
+    id: 19,
+    gambar: Tools19,
+    nama: "Postman",
+    ket: "API Testing",
+    dad: "1900",
+  },
+  {
+    id: 20,
+    gambar: Tools20,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "2000",
+  },
+  {
+    id: 21,
+    gambar: Tools21,
     nama: "Figma",
     ket: "Design App",
-    dad: "1100",
+    dad: "2100",
+  },
+  {
+    id: 22,
+    gambar: Tools22,
+    nama: "Vue Js",
+    ket: "Framework",
+    dad: "2200",
   },
 ];
 
