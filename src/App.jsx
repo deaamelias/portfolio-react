cfcfbad626c0a8231cfcd0903b4bfa99acc92ae0
deaamelias <deamelyaa07@gmail.com>
2025-09-15 23:44:25 +0700

    feat: add personal description section with project stats

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,38 @@ function App() {
           className="w-[500px] md:ml-auto"
         />
       </div>
+
+      <div className="tentang mt-32 py-10">
+        <div className="xl:w-2/3 lg:w-3/4 w-full mx-auto p-7 bg-zinc-800 rounded-lg">
+          <img
+            src={DataImage.HeroImage}
+            alt="Image"
+            className="w-12 rounded-md mb-10 sm:hidden"
+          />
+          <p className="text-base/loose mb-10">
+            Hi, perkenalkan saya Dea Amelia Setyorini, seorang Full Stack Web
+            Developer, Saya percaya bahwa desain dan fungsionalitas harus
+            berjalan beriringan, sehingga setiap proyek yang saya kembangkan
+            tidak hanya terlihat menarik tetapi juga memberikan pengalaman
+            pengguna yang optimal.
+          </p>
+          <div className="flex items-center justify-between">
+            <img
+              src={DataImage.HeroImage}
+              alt="Image"
+              className="w-12 rounded-md sm:block hidden"
+            />
+            <div className="flex items-center gap-6">
+              <div>
+                <h1 className="text-4xl mb-1">
+                  45<span className="text-violet-500">+</span>
+                </h1>
+                <p>Proyek Selesai</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
