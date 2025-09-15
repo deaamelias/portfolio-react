a46546eb1d7a3b6dac4bb388480c0571d53ebb6f
deaamelias <deamelyaa07@gmail.com>
2025-09-15 22:56:54 +0700

    feat: implement responsive Navbar with scroll behavior
    - Created Navbar component using React
    - Added menu items: Beranda, Tentang, Pendidikan, Proyek
    - Used scroll listener to toggle navbar visibility on scroll
    - Applied responsive styling with Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
   return (
     <>
-      <h1 className="text-4xl text-red-600 font-bold">hello </h1>
+      <h1 className="text-3xl">
+        asdhfvgehwjsdjxsajbxasxbasbxhdfgdjvsaJSBCSHCBSCHBSCHBSHCBSJZX
+        CJjbjjdsbvjdsbvjdsnvdsjnvdskcksdmcdshvusvcnsjdvsduvbasj
+      </h1>
     </>
   );
 }
